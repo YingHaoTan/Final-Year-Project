b23dd8ccbbf43302b05839f4f05b02ef05da390a
Ying Hao <C150124@e.ntu.edu.sg>
2019-02-17 01:08:45 +0800

    Changed global norm clip to 50.0

--- python/main.py
+++ python/main.py
@@ -97,7 +97,7 @@ lr = tf.case([(tf.less_equal(global_step, warmup_steps), lambda: warmup_lr),
              default=lambda: tf.convert_to_tensor(INITIAL_LR))
 optimizer = tf.train.RMSPropOptimizer(lr, decay=0.99, centered=True)
 grads = tf.gradients(loss, cmodel.variables)
-clipped_grads, global_norm = tf.clip_by_global_norm(grads, 20.0)
+clipped_grads, global_norm = tf.clip_by_global_norm(grads, 50.0)
 grads_n_vars = zip(clipped_grads, cmodel.variables)
 train_op = optimizer.apply_gradients(grads_n_vars, global_step=global_step)
 parameter_delta = [cmodel.variables[idx] - alt_model.variables[idx] for idx in range(len(cmodel.variables))]
